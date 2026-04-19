a667e235b51cdf600543e2c90020edff2cf784c1
macBookpro <macbookpro@chinaza.local>
2026-04-19 16:55:21 +0100

    update

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,34 @@
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
 require('dotenv').config();
 
+
+
+
+
+   const options = {
+      // logging: console.log,
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      logQueryParameters: true,
+      
+      dialectOptions: {
+        ssl: {
+          ca: fs.readFileSync('./certs/aiven-ca.pem'),
+          rejectUnauthorized: true,
+        },
+      },
+      /*  pool: {
+        max: 4, // Maximum number of connections in the poo
+        min: 0, // Minimum number of connections in  the pool
+        acquire: 30000, // The maximum time, in milliseconds, that pool will try to get a connection before throwing an error
+        idle: 10000, // The maximum time, in milliseconds, that a connection can be idle before being released
+      },*/
+    };
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -20,7 +48,9 @@ const sequelize = new Sequelize(
       timestamps: true,
       underscored: false
     }
-  }
+
+  },
+  options
 );
 
 module.exports = sequelize;
