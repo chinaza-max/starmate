0fef3eb5ee8e966027aead9cfdc8db0f00945b7f
macBookpro <macbookpro@chinaza.local>
2026-04-20 09:16:56 +0100

    update

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ async function startServer() {
 
 
 
-      await sequelize.sync({ alter: true }); 
+   //   await sequelize.sync({ alter: true }); 
 
     console.log('Database connection established successfully.');
  //  await sequelize.sync({ alter: true });  // Updates existing tables to match models (adds/changes columns without deleting data).
